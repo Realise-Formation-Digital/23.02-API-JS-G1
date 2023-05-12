7e77330aec486c69478ce887744fc3cd2f0c8643
24Syleo <sylvain.beggiora@outlook.com>
2023-05-12 11:50:11 +0200

    #firstjet ok

--- class/beerHandler.js
+++ class/beerHandler.js
@@ -2,6 +2,15 @@ import { urlBeers } from "../libs/const.js"
 
 class Beers {
 
+    async getBeersByName($value){
+        try{
+            let res = await axios.get(urlBeers + '?name=' + $value);
+            return res.data;
+        }
+        catch(e){
+            console.error(e);
+        }
+    }
 }
 
 
--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
-import Beer from "./beerHandler.js"
-import Ingredients from "./ingredientsHandler.js"
+import Beers from "../class/beerHandler.js";
+const beers = new Beers();
+
+let nameValue = document.getElementById("nameBeer");
+let nameButton = document.getElementById("nameBtn");
+
+nameButton.addEventListener("click", clickButton);
+
+
+async function clickButton(){
+    let nameSearch = nameValue.value;
+    let res = await beers.getBeersByName(nameSearch);
+     const rowEl = document.getElementById("rowList");
+     for(let i of res){
+        console.log(i);
+        const colEl = document.createElement('div');
+        colEl.classList.add('col');
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const img = document.createElement('img');
+        img.classList.add('card-img-top');
+        const body = document.createElement('div');
+        body.classList.add("card-body");
+        const heading = document.createElement('h5');
+        heading.classList.add('card-title');
+        const link = document.createElement('a');
+        link.classList.add('btn');
+        img.src = i.image_url;
+        heading.innerText = i.tagline;
+        link.innerText = i.name;
+        link.href = i.id;
+        rowEl.appendChild(colEl);
+        colEl.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(body);
+        body.appendChild(heading);
+        body.appendChild(link);
+     }
+}
