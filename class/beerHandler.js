499c1ee73516eae67d3f4d3a1135c3265fb42af5
Ariel <126970535+Aryel-Mag@users.noreply.github.com>
2023-05-12 09:40:45 +0200

    second project structure

---
+++ class/beerHandler.js
@@ -0,0 +1,8 @@
+import { urlBeers } from "../libs/const.js"
+
+class Beers {
+
+}
+
+
+export default Beers
---
+++ libs/const.js
@@ -0,0 +1,4 @@
+const urlBeers = "localhost:8765/beers"
+const urlIngredients = "localhost:8765/ingredients"
+
+export{urlBeers, urlIngredients}
