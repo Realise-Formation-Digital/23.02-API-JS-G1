b732223d26c13bb7e7a444190cfd0776902d031b
Yasm-ina <125749966+Yasm-ina@users.noreply.github.com>
2023-05-15 09:38:25 +0200

    Upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import Beers from "../class/beerHandler.js";
 import Ingredients from "../class/ingredientsHandler.js"
+//instance of object Beers
 const beers = new Beers();
 const ingredients = new Ingredients();
 
-
+//await state - response to obtain JSON beer from DB
+await beers.callBeers();
 let inputValue = document.getElementById("searchInput");
 let nameButton = document.getElementById("nameBtn");
 let typeButton = document.getElementById("typeBtn");
@@ -22,30 +24,4 @@ async function clickIng(){
     await ingredients.getBeersBytype(typeSearch);
 }
 
-//instance of object Beers
-const beer = new Beers();
-//await for response to obtain from Beers JSON from DB
-let res = await beer.callBeers();
-console.log(res);
- //link to html with id
- const colEl = document.getElementById('row-list');
- //loop array res
- for (let beer of res){
-    //create a div element
-     const divElement = document.createElement('div')
-     //injection of HTML card
-     colEl.innerHTML =
-     `<div class="card" style="width: 18rem;">
-     <img src="${beer.image_url}" class="card-img-top">
-     <div class="card-body">
-       <h5 class="card-title">${beer.name}</h5>
-       <p class="card-text">${beer.description}</p>
-     </div>
-   </div>` 
-
-   colElement.classList.add('col');
-   
- }
-
- document.getElementById("demo").innerHTML = text;
 
